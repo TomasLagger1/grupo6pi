59505a5887dd6a97c756016869304aedb9fda050
Zembo <jzemborain@udesa.edu.ar>
2025-04-03 17:54:51 -0300

    ahi creé las rutas para todas las paginas con sus controladores

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,5 +4,11 @@ const router = express.Router()
 const mainController = require('../controllers/mainController')
 
 router.get('/', mainController.index)
+router.get('/login', mainController.login)
+router.get('/product', mainController.product)
+router.get('/product-add', mainController.productAdd)
+router.get('/profile', mainController.profile)
+router.get('/register', mainController.register)
+router.get('/search-results', mainController.searchResults)
 
 module.exports = router
