4b79d52a6f11316d2e5fe131f2d0ba0251e368a7
Zembo <jzemborain@udesa.edu.ar>
2025-04-12 16:54:23 -0300

    muestra de productos en el home page

--- myapp/controllers/mainController.js
+++ myapp/controllers/mainController.js
@@ -2,7 +2,8 @@ const db = require("../db/datos");
 
 const mainController = {
     index: function (req, res) {
-        return res.render('index')
+        let imagenes = ['airpodsmax.png', 'airtags.jpg', 'apple-watch-ultra.jpg', 'homepod.jpg', 'img-macbook-pro-2019.jpg', 'img-samsung-galaxy-s10.jpg', 'img-tv-samsung-smart.jpg', 'ipadpro.png', 'iphone16.jpg', 'macbook-pro.jpg', 'magic-keyboard.jpg', 'studio-display.png', 'visionpro.jpg']
+        return res.render('index', {imagenes})
     },
     searchResults: function (req, res) {
         return res.render('searchResults')
