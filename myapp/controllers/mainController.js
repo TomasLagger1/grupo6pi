61ea13b2e88c759d6679fed024839d1d8567d927
TomasLagger1 <tlagger@udesa.edu.ar>
2025-04-12 18:16:16 -0300

    wuednx2eubxncp2iuvb

--- myapp/controllers/mainController.js
+++ myapp/controllers/mainController.js
@@ -3,7 +3,8 @@ const db = require("../db/datos");
 const mainController = {
     index: function (req, res) {
         let imagenes = ['airpodsmax.png', 'airtags.jpg', 'apple-watch-ultra.jpg', 'homepod.jpg', 'img-macbook-pro-2019.jpg', 'img-samsung-galaxy-s10.jpg', 'img-tv-samsung-smart.jpg', 'ipadpro.png', 'iphone16.jpg', 'macbook-pro.jpg', 'magic-keyboard.jpg', 'studio-display.png', 'visionpro.jpg']
-        return res.render('index', {imagenes})
+        const proddd = db.productos
+        return res.render('index', {imagenes, proddd})
     },
     searchResults: function (req, res) {
         return res.render('searchResults')
